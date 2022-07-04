d34a6fec651d57ac074812dd43d4daae8ab2b14c
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-07-04 17:12:11 +0300

    News Screen Page

--- navigation/screens/NewsScreen.js
+++ navigation/screens/NewsScreen.js
@@ -1,12 +1,94 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, SafeAreaView, Image, StyleSheet, Clipboard, TouchableOpacity, Dimensions } from 'react-native';
+import ProductCard from '../../components/ProductCard';
+import { news_list } from '../../sample-data/products';
+import { FlatList } from 'react-native-gesture-handler';
+import COLORS from '../../sample-data/COLORS';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import call from 'react-native-phone-call';
 
+const width = Dimensions.get('screen').width - 40
+
+const data = news_list
+const app_logo = require('../../assets/app.png')
+const img = require('../../assets/yonatan.jpg')
 export default function NewsScreen({ navigation }) {
+    const [phone, setPhone] = React.useState('+251964359872')
+
+    React.useLayoutEffect(() => {
+        navigation.setOptions({headerShown: false});
+      }, [navigation])
+
+    const forwardToCall = async () => {
+        const args = {
+            number: phone, // String value with the number to call
+            prompt: false, // Optional boolean property. Determines if the user should be prompted prior to the call 
+            skipCanOpen: true // Skip the canOpenURL check
+        }
+        call(args).catch(console.error)  
+    }
+    const Card = ({news}) => {
+        console.log(news)
+        return (
+            <TouchableOpacity style={{ flex: 1, flexDirection: 'row', backgroundColor: COLORS.light, marginBottom: 5}}
+                onPress={() => navigation.navigate('NewsDetail')}>
+                <View style={{ maxHeight: 100, alignItems: 'center', justifyContent: 'center'}}>
+                    <Image style={{ flex: 1, resizeMode: 'contain', maxWidth: width/2 -70}} source={news.img}/>
+                </View>
+                <View style={{paddingHorizontal: 10}}>
+                    <Text style={{ fontSize: 22, fontWeight: 'bold', color: COLORS.green}}>{news.name}</Text>
+                    <Text numberOfLines={4} style={{fontSize:11, marginRight: 85}}>{news.about}</Text>
+                </View>                
+            </TouchableOpacity>
+        )
+    }
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>News Screen</Text>
-        </View>
+        <SafeAreaView style={{marginTop: 50,}}>
+            <View style={style.header}>
+                <Text style={{fontSize: 28, fontWeight: 'bold', color: COLORS.green}}>SamiDish</Text>
+                <View style={{
+                    width: 60,
+                    height: 60,
+                    backgroundColor: '#fff',
+                    alignItems: 'center'
+                }}>
+                    <Image source={app_logo} style={{flex: 1, resizeMode: 'center'}}/>
+                </View>            
+            </View>
+            <View>
+                
+            <FlatList 
+                style={{backgroundColor: COLORS.white, marginTop: 5, marginBottom: 100}}
+                showsVerticalScrollIndicator={true}
+                contentContainerStyle={{
+                    marginTop: 10,
+                    paddingBottom: 50,
+                    marginHorizontal: 10
+                }}
+                data={data} 
+                renderItem={({item}) => <Card news={item}/>}/>
+            </View>
+        </SafeAreaView>
     );
 }
+
+const style = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 10,
+        height: 60,
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    productCard: {
+        backgroundColor: COLORS.green,
+        width,
+        marginVertical: 10,
+        maxHeight: 550,
+        minHeight: 550,
+        marginBottom: 10 ,
+        marginLeft: 15,
+        borderRadius: 10
+    }
+})
