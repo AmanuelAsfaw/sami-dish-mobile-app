b44148c09e631e2c01b5ef36df9041f04b6975fa
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-07-05 10:00:33 +0300

    Dump Data

---
+++ sample-data/products.js
@@ -0,0 +1,239 @@
+const product_list = [
+    {
+        name : 'Tv and Speakers',
+        description: 'Used and new Tv and speakers to buy',
+        category : 'TV',
+        img: require('../assets/products/please.png'),
+        price: 43,
+        about: "ወድ የሳሚ ዲሽ ተከታታዮች በቅርብ ይዘን በምንመጣው አዲስ ነገር የዲሽ ስራን ለማዘመን ደፋ ቀና እያልን እንገኛለን በአዲሱ ስራችን ዲሽ ሰሪዎችን ተጠቃሚ የሚያደርግ አዲስ ነገር ይዘን ስለምንመጣ እኛም የዲሽ ሰሪዎችን እገዛ ስለምንፈልግ ከታች ባለው ሊንክ በሚጠይቃቹ መሰረት ተመዝገቡ       1– ትክክለኛ ሙሉ ስም ከታች ባለው ሊንክ ገብታቹ ተመዝገቡ ለሁለት ሳምንት ብቻ ነው የምንመዘግበው ከጨረስን በውሃላ ሁሉን ነገር የምናሳውቃቹ ይሁናል ለሁሉም ዲሽ ሰሪዎች አድርሱልኝ አመስጋናለሁ ሳሚ ዲሽ"
+    },
+    {
+        name : 'Product 2',
+        description: 'product 2 desc',
+        category : 'TV',
+        img: require('../assets/products/please2.png'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        name : 'Product 3',
+        description: 'product 3 desc',
+        category : 'TV',
+        img: require('../assets/products/software.png'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        name : 'Product 4',
+        description: 'product 4 desc',
+        category : 'TV',
+        img: require('../assets/products/TOP-CABLE-COAXIAL-DIGITAL-cobre.png'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        name : 'Product 5',
+        description: 'product 5 desc',
+        category : 'TV',
+        img: require('../assets/products/please.png'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        name : 'Product 6',
+        description: 'product 6 desc',
+        category : 'TV',
+        img: require('../assets/products/please.png'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        name : 'Product 7',
+        description: 'product 7 desc',
+        category : 'TV',
+        img: require('../assets/products/please.png'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        name : 'Product 8',
+        description: 'product 8 desc',
+        category : 'TV',
+        img: require('../assets/products/please.png'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        name : 'Product 9',
+        description: 'product 9 desc',
+        category : 'TV',
+        img: require('../assets/products/product1.jpg'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    }
+]
+export default  product_list
+export const news_list = [
+    {
+        id: 1,
+        name : 'GOLDSTAR 7200HD',
+        description: 'Used and new Tv and speakers to buy',
+        category : 'TV',
+        img: require('../assets/News/4.jpg'),
+        price: 43,
+        about: "ወድ የሳሚ ዲሽ ተከታታዮች በቅርብ ይዘን በምንመጣው አዲስ ነገር የዲሽ ስራን ለማዘመን ደፋ ቀና እያልን እንገኛለን በአዲሱ ስራችን ዲሽ ሰሪዎችን ተጠቃሚ የሚያደርግ አዲስ ነገር ይዘን ስለምንመጣ እኛም የዲሽ ሰሪዎችን እገዛ ስለምንፈልግ ከታች ባለው ሊንክ በሚጠይቃቹ መሰረት ተመዝገቡ       1– ትክክለኛ ሙሉ ስም ከታች ባለው ሊንክ ገብታቹ ተመዝገቡ ለሁለት ሳምንት ብቻ ነው የምንመዘግበው ከጨረስን በውሃላ ሁሉን ነገር የምናሳውቃቹ ይሁናል ለሁሉም ዲሽ ሰሪዎች አድርሱልኝ አመስጋናለሁ ሳሚ ዲሽ"
+    },
+    {
+        id: 2,
+        name : 'GOLDSTAR 7200HD',
+        description: 'product 2 desc',
+        category : 'TV',
+        img: require('../assets/News/6.jpg'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        id: 3,
+        name : 'GOLDSTAR 7200HD',
+        description: 'product 3 desc',
+        category : 'TV',
+        img: require('../assets/products/software.png'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        id: 4,
+        name : 'GOLDSTAR 7200HD',
+        description: 'product 4 desc',
+        category : 'TV',
+        img: require('../assets/products/TOP-CABLE-COAXIAL-DIGITAL-cobre.png'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        id: 5,
+        name : 'GOLDSTAR 7200HD',
+        description: 'product 5 desc',
+        category : 'TV',
+        img: require('../assets/products/please.png'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        id: 6,
+        name : 'Product 6',
+        description: 'product 6 desc',
+        category : 'TV',
+        img: require('../assets/products/please.png'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        id: 7,
+        name : 'Product 7',
+        description: 'product 7 desc',
+        category : 'TV',
+        img: require('../assets/products/please.png'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        id: 8,
+        name : 'Product 8',
+        description: 'product 8 desc',
+        category : 'TV',
+        img: require('../assets/products/please.png'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        id: 9,
+        name : 'Product 9',
+        description: 'product 9 desc',
+        category : 'TV',
+        img: require('../assets/products/product1.jpg'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    }
+]
+
+export const technicians_list = [
+    {
+        id: 1,
+        name : 'Yonatan Tiliksaw',
+        region: 'Addis Ababa',
+        city : 'Addis Ababa',
+        img: require('../assets/yonatan.jpg'),
+        price: 43,
+        about: "ወድ የሳሚ ዲሽ ተከታታዮች በቅርብ ይዘን በምንመጣው አዲስ ነገር የዲሽ ስራን ለማዘመን ደፋ ቀና እያልን እንገኛለን በአዲሱ ስራችን ዲሽ ሰሪዎችን ተጠቃሚ የሚያደርግ አዲስ ነገር ይዘን ስለምንመጣ እኛም የዲሽ ሰሪዎችን እገዛ ስለምንፈልግ ከታች ባለው ሊንክ በሚጠይቃቹ መሰረት ተመዝገቡ       1– ትክክለኛ ሙሉ ስም ከታች ባለው ሊንክ ገብታቹ ተመዝገቡ ለሁለት ሳምንት ብቻ ነው የምንመዘግበው ከጨረስን በውሃላ ሁሉን ነገር የምናሳውቃቹ ይሁናል ለሁሉም ዲሽ ሰሪዎች አድርሱልኝ አመስጋናለሁ ሳሚ ዲሽ"
+    },
+    {
+        id: 2,
+        name : 'Naol Geresu',
+        region: 'Addis Ababa',
+        city : 'Addis Ababa',
+        img: require('../assets/naol.jpg'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        id: 3,
+        name : 'Abrsh Yeima',
+        region: 'Oromia',
+        category : 'Adama',
+        img: require('../assets/abrish.jpg'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        id: 4,
+        name : 'Ashenafi Teshale',
+        region: 'Oromia',
+        city : 'Adama',
+        img: require('../assets/ashenfi.jpg'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        id: 5,
+        name : 'Yonatan Tiliksaw',
+        region: 'Addis Ababa',
+        city : 'Addis Ababa',
+        img: require('../assets/yonatan.jpg'),
+        price: 43,
+        about: "ወድ የሳሚ ዲሽ ተከታታዮች በቅርብ ይዘን በምንመጣው አዲስ ነገር የዲሽ ስራን ለማዘመን ደፋ ቀና እያልን እንገኛለን በአዲሱ ስራችን ዲሽ ሰሪዎችን ተጠቃሚ የሚያደርግ አዲስ ነገር ይዘን ስለምንመጣ እኛም የዲሽ ሰሪዎችን እገዛ ስለምንፈልግ ከታች ባለው ሊንክ በሚጠይቃቹ መሰረት ተመዝገቡ       1– ትክክለኛ ሙሉ ስም ከታች ባለው ሊንክ ገብታቹ ተመዝገቡ ለሁለት ሳምንት ብቻ ነው የምንመዘግበው ከጨረስን በውሃላ ሁሉን ነገር የምናሳውቃቹ ይሁናል ለሁሉም ዲሽ ሰሪዎች አድርሱልኝ አመስጋናለሁ ሳሚ ዲሽ"
+    },
+    {
+        id: 6,
+        name : 'Naol Geresu',
+        region: 'Addis Ababa',
+        city : 'Addis Ababa',
+        img: require('../assets/naol.jpg'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        id: 7,
+        name : 'Abrsh Yeima',
+        region: 'Oromia',
+        category : 'Adama',
+        img: require('../assets/abrish.jpg'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+    {
+        id: 8,
+        name : 'Ashenafi Teshale',
+        region: 'Oromia',
+        city : 'Adama',
+        img: require('../assets/ashenfi.jpg'),
+        price: 43,
+        about: 'እስካሁን በትክክል የተመዘገቡ ዲሽ ሰሪዎች ናቸው እኛ ምንገልገው 1000 በላይ ስለሆነ አሁንም ያልተመዘገባቹ ከላይ ባለው ሊንክ ተመዝገቡ '
+    },
+]
+
+const image_list = [
+    require('../assets/products/please.png'),
+    require('../assets/products/TOP-CABLE-COAXIAL-DIGITAL-cobre.png'),
+    require('../assets/products/software.png')
+]
