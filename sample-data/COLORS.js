f8706e63e2086d6b41bb9e9071ae209ff8837952
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-07-05 10:00:24 +0300

    Theme Colors

---
+++ sample-data/COLORS.js
@@ -0,0 +1,11 @@
+const COLORS = {
+    white: '#fff',
+    dark: '#000',
+    red: '#F52A2A',
+    light: '#F1F1F1',
+    green: '#00B761',
+    light_green : '#C4E7D5',
+    gray: '#808080'
+  };
+  
+  export default COLORS;
