30c753150f0770509830a267a8062a41d3b0192d
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-07-05 15:34:27 +0300

    Change to large News-Card

--- navigation/screens/NewsScreen.js
+++ navigation/screens/NewsScreen.js
@@ -30,16 +30,28 @@ export default function NewsScreen({ navigation }) {
     const Card = ({news}) => {
         console.log(news)
         return (
-            <TouchableOpacity style={{ flex: 1, flexDirection: 'row', backgroundColor: COLORS.light, marginBottom: 5}}
-                onPress={() => navigation.navigate('NewsDetail')}>
-                <View style={{ maxHeight: 100, alignItems: 'center', justifyContent: 'center'}}>
-                    <Image style={{ flex: 1, resizeMode: 'contain', maxWidth: width/2 -70}} source={news.img}/>
-                </View>
+            <View style={{ flex: .1, flexDirection: 'column', backgroundColor: COLORS.light, borderRadius: 5, marginBottom: 5}}
+                >
+                
                 <View style={{paddingHorizontal: 10}}>
                     <Text style={{ fontSize: 22, fontWeight: 'bold', color: COLORS.green}}>{news.name}</Text>
-                    <Text numberOfLines={4} style={{fontSize:11, marginRight: 85}}>{news.about}</Text>
+                    <View style={{ marginVertical: 5, alignItems: 'flex-start', justifyContent: 'space-evenly', minHeight: 100, maxHeight: 250, backgroundColor: COLORS.dark}}>
+                        <Image style={{ flex: 1, resizeMode: 'cover', maxWidth: '100%'}} source={news.img}/>
+                    </View>
+                    <Text numberOfLines={4} lineHeight={10} style={{fontSize:11, marginRight: 0, color: COLORS.dark}}>{news.about}</Text>
+                    <TouchableOpacity style={{
+                        alignItems: 'center',
+                        margin: 5,
+                        padding: 5,
+                        borderColor: COLORS.green,
+                        borderWidth: 2,
+                        borderRadius: 8
+                    }}
+                    onPress={() => navigation.navigate('NewsDetail')}>
+                        <Text style={{ color: COLORS.green, fontSize: 20, fontWeight: '500', fontStyle: 'normal'}}>Instant View</Text>
+                    </TouchableOpacity>
                 </View>                
-            </TouchableOpacity>
+            </View>
         )
     }
     return (
