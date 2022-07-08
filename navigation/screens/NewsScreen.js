5c216f6ee42a7c21c5a30cc5c5b2ae72a140867e
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-07-08 17:12:17 +0300

    Fetch news-list for News Page

--- navigation/screens/NewsScreen.js
+++ navigation/screens/NewsScreen.js
@@ -6,6 +6,11 @@ import { FlatList } from 'react-native-gesture-handler';
 import COLORS from '../../sample-data/COLORS';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import call from 'react-native-phone-call';
+import { DOMAIN_NAME, FETCH_NEWS_LIST } from '../../sample-data/constants';
+
+import axios from 'axios';
+
+import LottieView from 'lottie-react-native';
 
 const width = Dimensions.get('screen').width - 40
 
@@ -14,31 +19,56 @@ const app_logo = require('../../assets/app.png')
 const img = require('../../assets/yonatan.jpg')
 export default function NewsScreen({ navigation }) {
     const [phone, setPhone] = React.useState('+251964359872')
+    const [newsList, setNewsList] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
 
     React.useLayoutEffect(() => {
         navigation.setOptions({headerShown: false});
       }, [navigation])
 
-    const forwardToCall = async () => {
-        const args = {
-            number: phone, // String value with the number to call
-            prompt: false, // Optional boolean property. Determines if the user should be prompted prior to the call 
-            skipCanOpen: true // Skip the canOpenURL check
-        }
-        call(args).catch(console.error)  
+    React.useEffect(() => {
+        console.log('use effect');
+        getNewsList()
+    }, [])
+    async function getNewsList(){
+        setLoading(true)
+        axios.get(FETCH_NEWS_LIST)
+        .then((response) => {
+            console.log(response)
+            console.log('News list featch response ')
+            console.log(response.data)
+            if(response.data &&  response.status && response.data.success){
+                console.log(response.data.news_data)
+                setLoading(false)
+                setNewsList(response.data.news_data)
+            }
+        })
+        .catch((error) => {
+            console.log('Error: News list feath error')
+            setLoading(false)
+            console.log(error.message)
+        })
     }
+    
     const Card = ({news}) => {
-        console.log(news)
+        var image_uri;
+        if(news.news_files.length > 0){
+            console.log(DOMAIN_NAME + news.news_files[0].file);
+            image_uri = {uri: DOMAIN_NAME + news.news_files[0].file}
+        }else{
+            var image_uri = require('../../assets/notfound.jpg')
+        }
+
         return (
             <View style={{ flex: .1, flexDirection: 'column', backgroundColor: COLORS.light, borderRadius: 5, marginBottom: 5}}
                 >
                 
                 <View style={{paddingHorizontal: 10}}>
-                    <Text style={{ fontSize: 22, fontWeight: 'bold', color: COLORS.green}}>{news.name}</Text>
-                    <View style={{ marginVertical: 5, alignItems: 'flex-start', justifyContent: 'space-evenly', minHeight: 100, maxHeight: 250, backgroundColor: COLORS.dark}}>
-                        <Image style={{ flex: 1, resizeMode: 'cover', maxWidth: '100%'}} source={news.img}/>
+                    <Text style={{ fontSize: 22, fontWeight: 'bold', color: COLORS.green}}>{news.title}</Text>
+                    <View style={{ marginVertical: 5, alignItems: 'center', justifyContent: 'center', minHeight: 100, maxHeight: 250, backgroundColor: COLORS.dark}}>
+                        <Image style={{ flex: 1, resizeMode: 'cover', maxWidth: '100%', minWidth: width - 20, height: '100%'}} source={image_uri}/>
                     </View>
-                    <Text numberOfLines={4} lineHeight={10} style={{fontSize:11, marginRight: 0, color: COLORS.dark}}>{news.about}</Text>
+                    <Text numberOfLines={4} lineHeight={10} style={{fontSize:14, marginRight: 0, color: COLORS.dark}}>{news.description}</Text>
                     <TouchableOpacity style={{
                         alignItems: 'center',
                         margin: 5,
@@ -47,7 +77,12 @@ export default function NewsScreen({ navigation }) {
                         borderWidth: 2,
                         borderRadius: 8
                     }}
-                    onPress={() => navigation.navigate('NewsDetail')}>
+                    onPress={() => {
+                        const news_image_list = []
+                        news.news_files.forEach(element => {
+                            news_image_list.push(DOMAIN_NAME+element.file)
+                        });
+                        navigation.navigate('NewsDetail', {news: news, imageList : news_image_list})}}>
                         <Text style={{ color: COLORS.green, fontSize: 20, fontWeight: '500', fontStyle: 'normal'}}>Instant View</Text>
                     </TouchableOpacity>
                 </View>                
@@ -69,7 +104,7 @@ export default function NewsScreen({ navigation }) {
             </View>
             <View>
                 
-            <FlatList 
+            {!loading && (<FlatList 
                 style={{backgroundColor: COLORS.white, marginTop: 5, marginBottom: 100}}
                 showsVerticalScrollIndicator={true}
                 contentContainerStyle={{
@@ -77,8 +112,15 @@ export default function NewsScreen({ navigation }) {
                     paddingBottom: 50,
                     marginHorizontal: 10
                 }}
-                data={data} 
-                renderItem={({item}) => <Card news={item}/>}/>
+                data={newsList} 
+                renderItem={({item}) => <Card news={item}/>}/>)}
+            {loading && (
+                <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginVertical: 150}}>
+                    <LottieView source={require('../../assets/98657-loader.json')} 
+                        autoPlay loop size={150} autoSize={true} 
+                        style={{width: 140, color: COLORS.white}} color={COLORS.white}/>
+                </View>
+            )}
             </View>
         </SafeAreaView>
     );
--- sample-data/constants.js
+++ sample-data/constants.js
@@ -1,3 +1,5 @@
 export const DOMAIN_NAME = 'https://sami-dish.herokuapp.com'
 
 export const FETCH_SOFTWARE_LIST = DOMAIN_NAME+'/mobile_api/softwares'
+
+export const FETCH_NEWS_LIST = DOMAIN_NAME+'/mobile_api/news'
