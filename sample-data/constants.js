34521fcb6cd15e2620d92319f10db8d7de194151
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-07-11 17:16:11 +0300

    Fetch Url lists

--- sample-data/constants.js
+++ sample-data/constants.js
@@ -1,5 +1,14 @@
-export const DOMAIN_NAME = 'https://sami-dish.herokuapp.com'
+// export const DOMAIN_NAME = 'https://sami-dish.herokuapp.com'
+// export const DOMAIN_NAME = 'http://172.20.23.221:8000'
+
+export const DOMAIN_NAME = 'https://samidish.info'
 
 export const FETCH_SOFTWARE_LIST = DOMAIN_NAME+'/mobile_api/softwares'
 
 export const FETCH_NEWS_LIST = DOMAIN_NAME+'/mobile_api/news'
+
+export const FETCH_TECHNICIANS_LIST = DOMAIN_NAME + '/mobile_api/technicians'
+
+export const FETCH_HOME_CATEGORY_LIST = DOMAIN_NAME + '/mobile_api/categories'
+
+export const FETCH_HOME_PRODUCT_LIST = DOMAIN_NAME + '/mobile_api/products/'
